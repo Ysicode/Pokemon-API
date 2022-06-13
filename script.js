2858b53b2f2dd13983ff3f063a0038b968530ed9
Simon Weiss <simonweiss.ml@gmail.com>
2022-06-13 20:36:44 +0200

    pokemon card

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 
 
  async function loadPokemon() {
-     let url = 'https://pokeapi.co/api/v2/pokemon/242';
+     let url = 'https://pokeapi.co/api/v2/pokemon/510';
      let response = await fetch(url);
      currentPokemon = await response.json();
      console.log('loaded', currentPokemon);
 
-     renderPokemonInfo();
+     renderPokemonCard();
  }
 
- function renderPokemonInfo() {
+ function renderPokemonCard() {
     let types = currentPokemon['types'];
     let type = currentPokemon['types'][0]['type']['name'];
     document.getElementById('pokemon_card_top').classList.add(`type_${type}`)
@@ -27,7 +27,36 @@
             </div>
             `;
      }
+     renderPokemonInfo();
  }
+
+ function renderPokemonInfo() {
+    getWeights();
+    getHeights();
+    getMoves();
+ }
+
+ function getMoves() {
+    let moves = currentPokemon['moves'];
+    document.getElementById('moves').innerHTML = moves.length;
+ }
+
+ function getHeights() {
+     let cm = currentPokemon['height'];
+     let ft =  cm * 0.0328084;
+     document.getElementById('height_cm').innerHTML = `${cm}cm`;
+     document.getElementById('height_ft').innerHTML = `${ft.toFixed(2)}ft`;
+ }
+
+ function getWeights() {
+    let kilo = currentPokemon['weight'];
+    let pounds = kilo * 2.20462
+    document.getElementById('weight_kg').innerHTML = `${currentPokemon['weight']}kg`;
+    document.getElementById('weight_lbs').innerHTML = ` ${Math.floor(pounds.toFixed(2))}lbs`;
+ }
+
+
+
 
  function likePokemon() {
      document.getElementById('heart_outline').classList.toggle('d-none');
